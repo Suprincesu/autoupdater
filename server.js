74ec247280863caab7536f7295ebf267131308e7
suprince shakya <suprince@hotspot.rts>
2018-05-06 11:18:30 +0545

    third commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const updater=require('./autoupdate');
 var app=express();
 
 console.log('teri ma ki aakh');
+console.log('teri bap ki laat');
+
 app.listen(8008,()=>{
 	console.log('listening to port 8008');
 });
